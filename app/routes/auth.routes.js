8febe33d791d4471fe60314f9b4e90db9a5067bc
ccarapezza <carapezza.christian@gmail.com>
2022-06-25 06:21:49 -0300

    package json fixed, and rollback some configs

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -3,9 +3,10 @@ const controller = require("../controllers/auth.controller");
 
 module.exports = function(app) {
   app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Headers", "x-access-token, x-access-hash, Origin, Content-Type, Accept");
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, x-access-hash, Origin, Content-Type, Accept"
+    );
     next();
   });
 
