d55cf0dfca486a2fdfa5b4064292683e5b613a48
ccarapezza <carapezza.christian@gmail.com>
2022-06-25 05:43:42 -0300

    cors fixed

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+app.options('*', cors());
 app.use(cors());
 
 // parse requests of content-type - application/json
