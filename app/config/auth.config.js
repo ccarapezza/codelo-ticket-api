2fdb1f6f8e4e2ac11f21fcdfff6c64235158004e
ccarapezza <carapezza.christian@gmail.com>
2022-06-26 02:00:15 -0300

    Last changes

--- app/config/auth.config.js
+++ app/config/auth.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  secret: "bezkoder-secret-key"
+  secret: "B99894B3A8B9EFF7F920F833FBA7EE73F1DBCE59F810401F5D771A6437976DAC"
 };
--- app/middleware/authJwt.js
+++ app/middleware/authJwt.js
@@ -2,7 +2,6 @@ const jwt = require("jsonwebtoken");
 const config = require("../config/auth.config.js");
 const db = require("../models");
 const User = db.user;
-const Ticket = db.ticket;
 
 verifyToken = (req, res, next) => {
   let token = req.headers["x-access-token"];
